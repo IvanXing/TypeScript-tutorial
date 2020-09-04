22dcfc414b4c4caec486e9a2ad0c7de463da1143
Ivan Xing <xingyifan10@qq.com>
2020-09-04 10:43:45 +0800

    属性装饰器

--- 05-04.属性的装饰器/src/index.ts
+++ 05-04.属性的装饰器/src/index.ts
@@ -1,13 +1,16 @@
-// function nameDecorator(target: any, key: string): any {
-//   const descriptor: PropertyDescriptor = {
-//     writable: false
-//   };
-//   return descriptor;
-// }
+function nameDecorator1(target: any, key: string): any {
+  // descriptor 返回 替代 原 descriptor
+  const descriptor: PropertyDescriptor = {
+    writable: false
+  };
+  return descriptor;
+}
+
 
 // test.name = 'dell lee';
 
 // 修改的并不是实例上的 name， 而是原型上的 name
+// target 是 prototype
 function nameDecorator(target: any, key: string): any {
   target[key] = 'lee';
 }
