e4ec6e88fd3f760ffe0fad5002a2895947f92d3f
Ivan Xing <xingyifan10@qq.com>
2020-09-04 10:32:41 +0800

    getter和setter的访问器

--- 05-03.访问器的装饰器/src/index.ts
+++ 05-03.访问器的装饰器/src/index.ts
@@ -1,3 +1,10 @@
+/*
+** 类中get和set访问器
+** get和set不能用同名的装饰器Decorator
+*/
+
+
+
 function visitDecorator(target: any, key: string, descriptor: PropertyDescriptor) {
   // descriptor.writable = false;
 }
@@ -10,7 +17,7 @@ class Test {
   get name() {
     return this._name;
   }
-  @visitDecorator
+  @visitDecorator           // 给set增加装饰器
   set name(name: string) {
     this._name = name;
   }
